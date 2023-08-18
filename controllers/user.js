4b7e95ce1dbc4d7ec8fd3e957af87f2bc3a99b5d
proyectosid23 <141842152+proyectosid23@users.noreply.github.com>
2023-08-18 17:14:04 -0500

    push code referencia

--- controllers/user.js
+++ controllers/user.js
@@ -23,9 +23,11 @@ const controller = {
 
             await User.create({ role, email, password, name, lastName, codReferido, codReferir, code, verified, logged, saldoActual, planes })
             const refUser = await User.findOne({codReferir: codReferido})
+            refUser.referidos.push(code)
+            refUser.save()
             await accountVerificationEmail(email, code)
             return userSignedUpResponse(req, res)
-            
+
         } catch (error) {
             next(error)
         }
