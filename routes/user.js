22ef9985ecac0fcd2cd4666dcef36ce6accd3474
proyectosid23 <141842152+proyectosid23@users.noreply.github.com>
2023-09-02 19:35:36 -0500

    middleware

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ const mustSignIn = require('../middlewares/mustSignIn')
 const { register, verify, login, loginWithToken, logout, readOne, update, read, destroy } = require('../controllers/user');
 
 router.post('/sign-up',validator(schema), accountExistsSignUp, codReferidoNotValidMid, register);
-router.post('/sign-in', validator(schemaSignin), accountExistsSignIn, accountHasBeenVerified, accountIsAlredyLogged, login)
+router.post('/sign-in', validator(schemaSignin), accountExistsSignIn, accountHasBeenVerified, login)
 router.post('/sign-out', passport.authenticate('jwt', { session: false }), logout)
 router.get('/', read )
 router.delete('/:id', passport.authenticate('jwt', { session: false }), destroy)
